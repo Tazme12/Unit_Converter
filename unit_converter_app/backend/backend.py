4434d00bd4d5cc829e17db96113b387816ba94b9
Tazme12 <camthornton.07@gmail.com>
2025-05-13 08:59:34 +0100

    hello

--- unit_converter_app/backend/backend.py
+++ unit_converter_app/backend/backend.py
@@ -1,19 +1,25 @@
-convert_to = input("Please enter a unit to convert to: ").lower()
-convert_from = input("Please enter a unit to convert from: ").lower()
-amount = int(input(f"Please enter the amount of {convert_from}: "))
+while True:
+    convert_to = input("Please enter a unit to convert to: ").lower()
+    convert_from = input("Please enter a unit to convert from: ").lower()
+    amount = int(input(f"Please enter the amount of {convert_from}: "))
 
-if (convert_from in ['meters', 'm']) and (convert_to in ['centimeters', 'cm']):
-    result = amount * 100
-    print(result,"cm")
+    if (convert_from in ['meters', 'm']) and (convert_to in ['centimeters', 'cm']):
+        result = amount * 100
+        print(result,"cm")
 
-elif (convert_from in ['centimeters', 'cm']) and (convert_to in ['meters', 'm']):
-    result = amount / 100
-    print(result,"m")
+    elif (convert_from in ['centimeters', 'cm']) and (convert_to in ['meters', 'm']):
+        result = amount / 100
+        print(result,"m")
 
-elif (convert_from in ['centimeter', 'cm']) and (convert_to in ['millimetre', 'mm']):
-    result = amount * 10
-    print(result,"mm")
+    elif (convert_from in ['centimeter', 'cm']) and (convert_to in ['millimetre', 'mm']):
+        result = amount * 10
+        print(result,"mm")
 
-elif (convert_from in ['millimetre', 'mm']) and (convert_to in ['centimeter', 'cm']):
-    result = amount / 10
-    print(result,'cm')
+    elif (convert_from in ['millimetre', 'mm']) and (convert_to in ['centimeter', 'cm']):
+        result = amount / 10
+        print(result,'cm')
+
+
+
+    else:
+        print("Sorry, that unit is not available")
