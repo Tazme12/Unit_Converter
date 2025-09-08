b925ff73f3dfc5f22b2d655f3eaac36a289fb050
Tazme12 <camthornton.07@gmail.com>
2025-09-08 08:59:23 +0100

    added flask

--- unit_converter_app/backend/backend.py
+++ unit_converter_app/backend/backend.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(debug = True)
 
 def convert_length(unit, unit_from, unit_to):
 
